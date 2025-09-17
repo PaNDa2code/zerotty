3899f583c8800c8afa599f295ed45be67d5f2a26
PaNDa2Code <moaaz0688@gmail.com>
2025-09-17 15:37:20 +0300

    build: edit harfbuzz build.zig

--- libs/harfbuzz/build.zig
+++ libs/harfbuzz/build.zig
@@ -5,6 +5,7 @@ pub fn build(b: *Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
+    const linkage = b.option(std.builtin.LinkMode, "linkage", "") orelse .static;
     const enable_freetype = b.option(bool, "enable_freetype", "Build Freetype") orelse true;
 
     const hurfbuzz_upstream = b.dependency("harfbuzz", .{});
@@ -14,34 +15,37 @@ pub fn build(b: *Build) void {
         .root_source_file = b.path("src/root.zig"),
         .target = target,
         .optimize = optimize,
+        .link_libcpp = true,
     });
+
+    harfbuzz_mod.addCSourceFile(.{
+        .file = hurfbuzz_upstream.path("src/harfbuzz.cc"),
+        .flags = &.{
+            if (enable_freetype) "-DHAVE_FREETYPE" else "",
+            "-DHB_NO_FEATURES_H",
+            "-std=c++11",
+            "-nostdlib++",
+            "-fno-exceptions",
+            "-fno-rtti",
+            "-fno-threadsafe-statics",
+            "-fvisibility-inlines-hidden",
+        },
+    });
+
+    harfbuzz_mod.addIncludePath(freetype_upstream.path("include"));
+    harfbuzz_mod.addIncludePath(hurfbuzz_upstream.path("src"));
 
     const harfbuzz_lib = b.addLibrary(.{
         .name = "harfbuzz",
-        .linkage = .static,
-        .root_module = b.createModule(.{
-            .target = target,
-            .optimize = optimize,
-        }),
+        .linkage = linkage,
+        .root_module = harfbuzz_mod,
     });
-
-    harfbuzz_lib.addCSourceFile(.{
-        .file = hurfbuzz_upstream.path("src/harfbuzz.cc"),
-        .flags = &.{if (enable_freetype) "-DHAVE_FREETYPE" else ""},
-    });
-
-    harfbuzz_lib.linkLibCpp();
 
     harfbuzz_lib.installHeadersDirectory(
         hurfbuzz_upstream.path("src/"),
         "harfbuzz",
         .{ .include_extensions = &.{".h"} },
     );
-    harfbuzz_lib.addIncludePath(freetype_upstream.path("include"));
-
-    harfbuzz_mod.addIncludePath(hurfbuzz_upstream.path("src"));
-    harfbuzz_mod.addIncludePath(freetype_upstream.path("include"));
-    harfbuzz_mod.linkLibrary(harfbuzz_lib);
 
     b.installArtifact(harfbuzz_lib);
 }
