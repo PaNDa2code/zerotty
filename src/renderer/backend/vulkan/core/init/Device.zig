83625bee4e2157bfbf6341eacebcb2bba12a58c9
PaNDa2code <moaaz0688@gmail.com>
2025-12-25 18:21:50 +0200

    feat: implement Device.init

--- src/renderer/backend/vulkan/core/init/Device.zig
+++ src/renderer/backend/vulkan/core/init/Device.zig
@@ -1,57 +1,197 @@
 const Device = @This();
 
+pub const PhysicalDevice = struct {
+    handle: vk.PhysicalDevice,
+    properties: vk.PhysicalDeviceProperties,
+    memory_properties: vk.PhysicalDeviceMemoryProperties,
+
+    support_present: bool,
+
+    graphic_family_index: u32,
+    present_family_index: u32,
+};
+
 handle: vk.Device,
-
-physical_device: vk.PhysicalDevice,
-physical_device_props: vk.PhysicalDeviceProperties,
-physical_device_memory_props: vk.PhysicalDeviceMemoryProperties,
-
-graphic_family_index: u32,
-present_family_index: u32,
+physical_device: PhysicalDevice,
 
 vkd: vk.DeviceWrapper,
 
 pub const InitError = std.mem.Allocator.Error ||
-    vk.InstanceWrapper.enumeratePhysicalDevicesError;
+    vk.InstanceWrapper.CreateDeviceError ||
+    vk.InstanceWrapper.EnumeratePhysicalDevicesError ||
+    vk.InstanceWrapper.GetPhysicalDeviceSurfaceSupportKHRError ||
+    error{NoSupportedGPU};
 
 pub fn init(
     allocator: std.mem.Allocator,
     instance: *const Instance,
-    surface: ?WsiSurface,
-    extensions: [][*:0]const u8,
+    surface: vk.SurfaceKHR, // optional
+    required_extensions: [][*:0]const u8,
 ) InitError!Device {
-    const physical_devices =
-        try instance.vki.enumeratePhysicalDevicesAlloc(
-            instance.handle,
-            allocator,
-        );
-
+    const physical_devices = try getPhysicalDevices(allocator, instance, surface);
     defer allocator.free(physical_devices);
 
-    std.sort.heap(vk.PhysicalDevice, physical_devices, &instance.vki, physicalDeviceGt);
+    for (physical_devices) |physical_device| {
+        if (surface != .null_handle and !physical_device.support_present)
+            continue;
 
+        const device =
+            createDevice(instance, physical_device, required_extensions) catch continue;
 
-    return .{};
-}
+        const vkGetDeviceProcAddr = instance.vki.dispatch.vkGetDeviceProcAddr.?;
+        const vkd = vk.DeviceWrapper.load(device, vkGetDeviceProcAddr);
 
-fn physicalDeviceScore(vki: *const vk.InstanceWrapper, physical_device: vk.PhysicalDevice) u32 {
-    var score: u32 = 0;
-    const device_props = vki.getPhysicalDeviceProperties(physical_device);
-    switch (device_props.device_type) {
-        // .discrete_gpu => score += 2_000,
-        .integrated_gpu => score += 1_000,
-        else => {},
+        return .{
+            .handle = device,
+            .physical_device = physical_device,
+
+            .vkd = vkd,
+        };
     }
-    // score += device_props.limits.max_image_dimension_2d;
 
-    return score;
+    return error.NoSupportedGPU;
 }
 
-fn physicalDeviceGt(vki: *const vk.InstanceWrapper, a: vk.PhysicalDevice, b: vk.PhysicalDevice) bool {
-    return physicalDeviceScore(vki, a) > physicalDeviceScore(vki, b);
+fn createDevice(
+    instance: *const Instance,
+    physical_device: PhysicalDevice,
+    required_extensions: [][*:0]const u8,
+) !vk.Device {
+    const queue_create_info = vk.DeviceQueueCreateInfo{
+        .queue_family_index = physical_device.graphic_family_index,
+        .queue_count = 1,
+        .p_queue_priorities = &.{1},
+    };
+
+    const layers =
+        if (builtin.mode == .Debug)
+            [_][*:0]const u8{"VK_LAYER_KHRONOS_validation"}
+        else
+            [_][*:0]const u8{};
+
+    const sync2_features = vk.PhysicalDeviceSynchronization2Features{
+        .synchronization_2 = .true,
+    };
+
+    const device_features = vk.PhysicalDeviceFeatures{};
+
+    const device_create_info = vk.DeviceCreateInfo{
+        .p_next = @ptrCast(&sync2_features),
+
+        .queue_create_info_count = 1,
+        .p_queue_create_infos = @ptrCast(&queue_create_info),
+
+        .enabled_extension_count = required_extensions.len,
+        .pp_enabled_extension_names = required_extensions.ptr,
+
+        .enabled_layer_count = layers.len,
+        .pp_enabled_layer_names = &layers,
+
+        .p_enabled_features = &device_features,
+    };
+
+    return instance.vki.createDevice(
+        physical_device.handle,
+        &device_create_info,
+        instance.vk_allocator,
+    );
+}
+
+inline fn getPhysicalDevices(
+    allocator: std.mem.Allocator,
+    instance: *const Instance,
+    surface: vk.SurfaceKHR,
+) ![]PhysicalDevice {
+    const handles = try instance.vki
+        .enumeratePhysicalDevicesAlloc(instance.handle, allocator);
+
+    defer allocator.free(handles);
+
+    var physical_devices = try allocator.alloc(PhysicalDevice, handles.len);
+
+    for (handles, 0..) |handle, i| {
+        physical_devices[i].support_present = false;
+
+        queryPhysicalDeviceInfo(instance, handle, &physical_devices[i]);
+        selectQueueFamilies(allocator, instance, handle, surface, &physical_devices[i]);
+    }
+
+    return physical_devices;
+}
+
+fn queryPhysicalDeviceInfo(
+    instance: *const Instance,
+    handle: vk.PhysicalDevice,
+    out: *PhysicalDevice,
+) void {
+    out.handle = handle;
+
+    out.properties =
+        instance.vki.getPhysicalDeviceProperties(handle);
+    out.features =
+        instance.vki.getPhysicalDeviceFeatures(handle);
+    out.memory_properties =
+        instance.vki.getPhysicalDeviceMemoryProperties(handle);
+}
+
+fn selectQueueFamilies(
+    allocator: std.mem.Allocator,
+    instance: *const Instance,
+    handle: vk.PhysicalDevice,
+    surface: vk.SurfaceKHR,
+    out: *PhysicalDevice,
+) !void {
+    const queue_props = try instance.vki
+        .getPhysicalDeviceQueueFamilyPropertiesAlloc(handle, allocator);
+    defer allocator.free(queue_props);
+
+    var graphics: ?u32 = null;
+    var present: ?u32 = null;
+    var graphics_present: ?u32 = null;
+
+    for (queue_props, 0..) |props, j| {
+        const idx: u32 = @intCast(j);
+
+        const graphics_support = props.queue_flags.graphics_bit;
+
+        const present_support =
+            try instance.vki.getPhysicalDeviceSurfaceSupportKHR(
+                handle,
+                idx,
+                surface,
+            ) == .true;
+
+        if (graphics_support and graphics == null)
+            graphics = idx;
+
+        if (present_support and present == null)
+            present = idx;
+
+        if (graphics and present) {
+            graphics_present = idx;
+            break;
+        }
+    }
+
+    if (graphics_present) {
+        out.graphic_family_index = graphics_present;
+        out.present_family_index = graphics_present;
+        out.support_present = true;
+        return;
+    }
+
+    if (present) |i| {
+        out.present_family_index = i;
+        out.support_present = true;
+    }
+
+    if (graphics) |i| {
+        out.graphic_family_index = i;
+    }
 }
 
 const std = @import("std");
+const builtin = @import("builtin");
 const vk = @import("vulkan");
 
 const Instance = @import("Instance.zig");
