cadfccbefa1babe07fb101b22ddc81dc22f81955
PaNDa2code <moaaz0688@gmail.com>
2026-01-16 16:28:03 +0200

    feat(vulkan): zig vertex data definition

---
+++ src/renderer/vulkan/rendering/vertex.zig
@@ -0,0 +1,37 @@
+pub const Vertex = packed struct {
+    quad_vertex: math.Vec4(f32), // postion, uv
+};
+
+pub const PackedPostion = packed struct(u32) { row: u16, col: u16 };
+
+pub const Instance = packed struct {
+    packed_pos: PackedPostion,
+    glyph_index: u32,
+    style_index: u32,
+};
+
+pub const GlyphMetrics = packed struct {
+    coord_start: math.Vec2(u32),
+    coord_end: math.Vec2(u32),
+    bearing: math.Vec2(i32),
+};
+
+pub const GlyphStyle = packed struct {
+    fg_color: color.ColorRGBAu8,
+    bg_color: color.ColorRGBAu8,
+};
+
+pub const Uniforms = packed struct {
+    cell_height: f32,
+    cell_width: f32,
+    screen_height: f32,
+    screen_width: f32,
+    atlas_cols: f32,
+    atlas_rows: f32,
+    atlas_width: f32,
+    atlas_height: f32,
+    descender: f32,
+};
+
+const math = @import("../../common/math.zig");
+const color = @import("../../common/color.zig");
