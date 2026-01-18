41b73086e06103d45a466ab9bafcfa8da4ed2118
PaNDa2code <moaaz0688@gmail.com>
2026-01-18 20:51:12 +0200

    feat(vulkan): create RenderContext

---
+++ src/renderer/vulkan/rendering/RenderContext.zig
@@ -0,0 +1,71 @@
+const RenderContext = @This();
+
+instance: *const core.Instance,
+device: *const core.Device,
+
+surface: vk.SurfaceKHR,
+
+// allocator: std.mem.Allocator,
+allocator_adapter: *core.memory.AllocatorAdapter,
+device_allocator: *core.memory.DeviceAllocator,
+
+pub fn init(allocator: std.mem.Allocator, window: anytype) !RenderContext {
+    const allocator_adapter = try core.memory.AllocatorAdapter.init(allocator);
+
+    const surface_creation_info = SurfaceCreationInfo.fromWindow(window);
+
+    const instance = try allocator.create(core.Instance);
+    instance.* = try core.Instance.init(
+        allocator,
+        &allocator_adapter.alloc_callbacks,
+        surface_creation_info.instanceExtensions(),
+    );
+    errdefer instance.deinit();
+
+    const surface = try createWindowSurface(instance, surface_creation_info);
+
+    const device = try allocator.create(core.Device);
+    device.* = try .init(
+        allocator,
+        instance,
+        surface,
+        SurfaceCreationInfo.deviceExtensions(),
+    );
+    errdefer device.deinit();
+
+    const device_allocator = try allocator.create(core.memory.DeviceAllocator);
+
+    device_allocator.* = .init(device, allocator);
+
+    return .{
+        .instance = instance,
+        .device = device,
+
+        .surface = surface,
+
+        .allocator_adapter = allocator_adapter,
+        .device_allocator = device_allocator,
+    };
+}
+
+pub fn deinit(self: *RenderContext) void {
+    const allocator = self.allocator_adapter.allocator;
+
+    self.device.deinit();
+    self.instance.deinit();
+
+    self.allocator_adapter.deinit();
+
+    allocator.destroy(self.instance);
+    allocator.destroy(self.device);
+    allocator.destroy(self.device_allocator);
+}
+
+const std = @import("std");
+const vk = @import("vulkan");
+
+const core = @import("../core/root.zig");
+
+const window_surface = @import("window_surface.zig");
+const SurfaceCreationInfo = window_surface.SurfaceCreationInfo;
+const createWindowSurface = window_surface.createWindowSurface;
---
+++ src/renderer/vulkan/rendering/window_surface.zig
@@ -0,0 +1,128 @@
+const std = @import("std");
+const vk = @import("vulkan");
+
+const build_options = @import("build_options");
+
+const Instance = @import("../core/root.zig").Instance;
+
+pub const SurfaceCreationInfo = union(enum) {
+    win32: struct {
+        hwnd: *anyopaque,
+        hinstance: *anyopaque,
+    },
+    xcb: struct {
+        connection: *anyopaque,
+        window: u32,
+    },
+    xlib: struct {
+        window: c_ulong,
+        dpy: *anyopaque,
+    },
+    glfw: struct {
+        window: *anyopaque,
+    },
+    headless: void,
+
+    pub fn fromWindow(window: anytype) SurfaceCreationInfo {
+        return switch (@TypeOf(window.*).system) {
+            .Win32 => .{
+                .win32 = .{ .hwnd = @ptrCast(window.hwnd), .hinstance = @ptrCast(window.h_instance) },
+            },
+            .Xcb => .{
+                .xcb = .{ .connection = @ptrCast(window.connection), .window = @intCast(window.window) },
+            },
+            .Xlib => .{
+                .xlib = .{ .window = window.w, .dpy = @ptrCast(window.display) },
+            },
+            .GLFW => .{
+                .glfw = .{
+                    .window = @ptrCast(window.window),
+                },
+            },
+            else => unreachable,
+        };
+    }
+
+    pub fn instanceExtensions(self: SurfaceCreationInfo) []const [*:0]const u8 {
+        if (build_options.@"window-system" == .glfw) {
+            var count: u32 = 0;
+            const extentions: [*]const [*:0]const u8 =
+                @ptrCast(c.glfwGetRequiredInstanceExtensions(&count));
+
+            return extentions[0..count];
+        }
+
+        var exts: [2][*:0]const u8 = undefined;
+
+        exts[0] = "VK_KHR_surface";
+
+        exts[1] = switch (self) {
+            .win32 => "VK_KHR_win32_surface",
+            .xcb => "VK_KHR_xcb_surface",
+            .xlib => "VK_KHR_xlib_surface",
+            .headless => "VK_EXT_headless_surface",
+            else => unreachable,
+        };
+
+        return &exts;
+    }
+
+    pub fn deviceExtensions() []const [*:0]const u8 {
+        return &.{
+            "VK_KHR_swapchain",
+        };
+    }
+};
+
+pub fn createWindowSurface(
+    instance: *const Instance,
+    surface_creation_info: SurfaceCreationInfo,
+) !vk.SurfaceKHR {
+    if (build_options.@"window-system" == .glfw) {
+        var surface: vk.SurfaceKHR = .null_handle;
+
+        _ = c.glfwCreateWindowSurface(
+            @ptrFromInt(@intFromEnum(instance.handle)),
+            @ptrCast(surface_creation_info.glfw.window),
+            @ptrCast(instance.vk_allocator),
+            @ptrCast(&surface),
+        );
+
+        return surface;
+    }
+
+    switch (surface_creation_info) {
+        .win32 => |info| {
+            const surface_info = vk.Win32SurfaceCreateInfoKHR{
+                .hwnd = @ptrCast(info.hwnd),
+                .hinstance = @ptrCast(info.hinstance),
+            };
+            return instance.vki.createWin32SurfaceKHR(instance.handle, &surface_info, instance.vk_allocator);
+        },
+        .xcb => |info| {
+            const surface_info = vk.XcbSurfaceCreateInfoKHR{
+                .connection = @ptrCast(info.connection),
+                .window = @intCast(info.window),
+            };
+            return instance.vki.createXcbSurfaceKHR(instance.handle, &surface_info, instance.vk_allocator);
+        },
+        .xlib => |info| {
+            const surface_info = vk.XlibSurfaceCreateInfoKHR{
+                .window = info.window,
+                .dpy = @ptrCast(info.dpy),
+            };
+            return instance.vki.createXlibSurfaceKHR(instance.handle, &surface_info, instance.vk_allocator);
+        },
+        .headless => {
+            const surface_info = vk.HeadlessSurfaceCreateInfoEXT{};
+            return instance.vki.createHeadlessSurfaceEXT(instance.handle, &surface_info, instance.vk_allocator);
+        },
+        else => unreachable,
+    }
+}
+
+const c = @cImport({
+    @cDefine("GLFW_INCLUDE_NONE", "");
+    @cDefine("GLFW_INCLUDE_VULKAN", "");
+    @cInclude("GLFW/glfw3.h");
+});
