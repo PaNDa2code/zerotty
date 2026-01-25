09c528c058b4e3449ee3657a590de9baf0619ef8
PaNDa2code <moaaz0688@gmail.com>
2026-01-25 08:56:47 +0200

    feat: window interface

--- src/window/root.zig
+++ src/window/root.zig
@@ -1,46 +1,60 @@
 pub const Api = @import("build_options").@"window-system";
 
-fn WindowInterface(WindowType: type) type {
+fn WindowInterface(WinType: type) type {
     return struct {
         const Self = @This();
 
-        window: WindowType,
+        comptime {
+            backendAssert(WinType);
+        }
 
-        pub fn initAlloc(allocator: std.mem.Allocator, title: []const u8, height: u32, width: u32) !*Self {
-            var window = try allocator.create(WindowType);
-            window.* = WindowType.new(title, height, width);
-            window.setup(allocator);
+        w: WinType,
+
+        pub fn initAlloc(allocator: std.mem.Allocator, options: WindowCreateOptions) !*Self {
+            const window = try allocator.create(Self);
+            window.* = WinType.new(options.title, options.height, options.width);
+            window.open(allocator);
             return window;
         }
 
         pub fn new(title: []const u8, height: u32, width: u32) Self {
             return .{
-                .window = WindowType.new(title, height, width),
+                .w = WinType.new(title, height, width),
             };
         }
 
         pub fn open(self: *Self, allocator: std.mem.Allocator) !void {
-            try self.window.open(allocator);
+            try self.w.open(allocator);
         }
 
         pub fn setTitle(self: *Self, title: []const u8) !void {
-            try self.window.setTitle(title);
+            try self.w.setTitle(title);
         }
 
         pub fn pumpMessages(self: *Self) void {
-            self.window.pumpMessages();
+            self.w.pumpMessages();
         }
 
         pub fn close(self: *Self) void {
-            self.window.close();
+            self.w.close();
         }
     };
 }
 
+pub const WindowCreateOptions = struct {
+    title: []const u8 = "zerotty",
+    height: u32,
+    width: u32,
+};
+
 pub const RenderCreateInfo = struct {
     handles: WindowHandles,
+    extent: Extent,
+};
+
+pub const Extent = struct {
     width: u32,
-    hieght: u32,
+    height: u32,
 };
 
 pub const WindowHandles = switch (Api) {
@@ -61,16 +75,32 @@ pub const WindowHandles = switch (Api) {
     },
 };
 
-
 pub const OpenGLContextCreateInfo = struct {};
+pub const GLESContextCreateInfo = struct {};
 
-const Impl = switch (Api) {
+const Win = switch (Api) {
     .win32 => @import("Win32.zig"),
     .xlib => @import("Xlib.zig"),
     .xcb => @import("Xcb.zig"),
     .glfw => @import("GLFW.zig"),
 };
 
-pub const Window = Impl;
+pub const Window = Win;
 
 const std = @import("std");
+
+fn backendAssert(comptime T: type) void {
+    comptime {
+        const methods = [_][]const u8{
+            "new",
+            "open",
+            "close",
+            "pumpMessages",
+            "setTitle",
+        };
+
+        for (methods) |method| {
+            if (!@hasDecl(T, method)) @compileError("Backend must define " ++ method);
+        }
+    }
+}
