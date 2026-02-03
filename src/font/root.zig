9611c64e47641faaa3f1d9c41fd91c3dbb8ab2cc
PaNDa2code <moaaz0688@gmail.com>
2026-02-03 15:12:21 +0200

    feat: packed GlyphAtlasEntry

--- src/font/root.zig
+++ src/font/root.zig
@@ -1,5 +1,26 @@
 pub const Atlas = @import("Atlas.zig");
 
+pub const GlyphAtlasEntry = packed struct(u64) {
+    // postion
+    atlas_id: u8, // max atlases count is 256
+    x: u12,
+    y: u12, // max 4096
+
+    // Metrics
+    width: u8,
+    height: u8, // max 255
+    x_bearing: i8,
+    y_bearing: i8, // max 127, min -128
+};
+
+pub const FontID = enum(u32) { _ };
+pub const GlyphIndex = enum(u32) { _ };
+
+pub const GlyphID = packed struct(u64) {
+    font: FontID,
+    index: GlyphIndex,
+};
+
 test Atlas {
     const std = @import("std");
     const allocator = std.testing.allocator;
