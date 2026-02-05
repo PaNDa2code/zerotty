4bebd2ac50762ab5e3b4f22ad7968499a9b5c1d8
PaNDa2Code <moaaz0688@gmail.com>
2026-02-06 00:13:16 +0200

    feat: simple bin packing for font cache

---
+++ src/font/bin_packing.zig
@@ -0,0 +1,75 @@
+pub const Rectangle = struct { x: u16, y: u16, height: u16, width: u16 };
+
+pub const Postion = struct { x: u16, y: u16 };
+
+pub const Shelf = struct {
+    current_width: u16,
+    height: u16,
+};
+
+/// Single Atlas Packer (Shelf Algorithm)
+///
+/// Origin is Top-Left at (0, 0).
+pub const Packer = struct {
+    max_height: u32,
+    max_width: u32,
+
+    shelfs: std.ArrayList(Shelf),
+
+    pub fn init(max_height: u32, max_width: u32) Packer {
+        return .{
+            .max_height = max_height,
+            .max_width = max_width,
+            .shelfs = .empty,
+        };
+    }
+
+    /// Finds the first available space in existing shelves
+    /// or creates a new one (First-Fit strategy).
+    pub fn findEmptyRectangle(
+        self: *Packer,
+        allocator: std.mem.Allocator,
+        height: u16,
+        width: u16,
+    ) ?Postion {
+        var height_tracker: u16 = 0;
+        for (self.shelfs.items, 0..) |*shelf, i| {
+            const is_last = i == self.shelfs.items.len - 1;
+
+            if (!is_last and height > shelf.height or
+                shelf.current_width + width > self.max_width or
+                (is_last and height_tracker + @max(height, shelf.height) > shelf.max_height))
+            {
+                height_tracker += shelf.height;
+                continue;
+            }
+
+            const x = shelf.current_width;
+            const y = height_tracker;
+
+            shelf.current_width += width;
+            shelf.height = @max(height, shelf.height);
+
+            return .{ .x = x, .y = y };
+        }
+
+        if (height_tracker + height > self.max_height)
+            return null;
+
+        const shelf = Shelf{
+            .height = height,
+            .current_width = width,
+        };
+
+        try self.shelfs.append(allocator, shelf);
+
+        return .{
+            .x = 0,
+            .y = height_tracker,
+        };
+    }
+};
+
+const std = @import("std");
+
+const root = @import("root.zig");
