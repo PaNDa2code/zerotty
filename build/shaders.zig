081862be548e4ccec0614f40acaaaf87f745718c
PaNDa2Code <moaaz0688@gmail.com>
2026-02-08 17:18:40 +0200

    build: small edits for shader build

--- build/shaders.zig
+++ build/shaders.zig
@@ -19,14 +19,13 @@ pub fn compiledShadersPathes(
         try findPathAlloc(b.allocator, "glslangValidator") != null and
         try findPathAlloc(b.allocator, "spirv-opt") != null;
 
-    var glslang: ?*Build.Dependency = null;
     var spirv_opt: ?*Build.Step.Compile = null;
     var glslangValidator: ?*Build.Step.Compile = null;
 
     if (!glslang_tools_installed) {
-        glslang = b.lazyDependency("glslang", .{ .optimize = .ReleaseFast }) orelse return &.{};
-        glslangValidator = glslang.?.artifact("glslangValidator");
-        spirv_opt = glslang.?.artifact("spirv-opt");
+        const glslang = b.lazyDependency("glslang", .{ .optimize = .ReleaseFast }) orelse return &.{};
+        glslangValidator = glslang.artifact("glslangValidator");
+        spirv_opt = glslang.artifact("spirv-opt");
     }
 
     for (files, 0..) |file, i| {
@@ -48,7 +47,6 @@ pub fn compiledShadersPathes(
         if (renderer == .opengl)
             glslangValidator_cmd.addArg("opengl");
 
-        // addPrefixedOutputFileArg not working with glslang
         glslangValidator_cmd.addArg("-o");
         const shader_spv_path = glslangValidator_cmd.addOutputFileArg(output_basename);
 
@@ -71,7 +69,9 @@ pub fn compiledShadersPathes(
             } else shader_spv_path;
 
         if (b.release_mode == .off)
-            glslangValidator_cmd.addArg("-gVS");
+            glslangValidator_cmd.addArg("-gVS")
+        else 
+            glslangValidator_cmd.addArg("-g0");
 
         shader_pathes[i] = .{
             .name = output_basename,
